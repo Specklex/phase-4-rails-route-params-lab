f7127e9c3976c1b81cb2e2c7dade671ff7bba43d
Jaden Lund <lundjaden@gmail.com>
2022-09-20 18:38:41 -0400

    commit tree 700fe4d3a74a45db68b56a927d306c14d0f6b174

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,41 @@
 class StudentsController < ApplicationController
+  # def index
+  #   if params[:name]
+  #     students =
+  #       Student.select { |student| student.first_name == params[:name] } ||
+  #         Student.select { |student| student.last_name == params[:name] }
+  #   else
+  #     students = Student.all
+  #   end
+  #   render json: students
+  # end
 
   def index
-    students = Student.all
+    # all the instances of a class
+    if params[:name]
+      students =
+        Student.select do |student|
+          student.first_name == params[:name] ||
+            student.last_name == params[:name]
+        end
+    else
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def grades
+    grades = Student.all.order(:grade).reverse
+    render json: grades
+  end
+
+  def highest_grade
+    grade = Student.all.order(:grade).reverse.first
+    render json: grade
+  end
 end
